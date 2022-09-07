39db0d88d03f756ab290ed1306aa116f951b2090
swesda <swesda@yandex.ru>
2022-09-07 18:48:26 +0300

    12 commit

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -27,7 +27,7 @@ public class CardOrderTest {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
         driver.get("http://localhost:9999");
     }
 
